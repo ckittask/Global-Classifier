35123547f2919f432ccf7bfd7796eff73500b113
erangi-ar <111747955+erangi-ar@users.noreply.github.com>
2025-05-27 21:58:04 +0530

    remove unused ESLint configuration and related files

--- Global-Classifier/GUI/src/components/Header/Header.test.tsx
+++
@@ -1,8 +0,0 @@
-const { render, screen } = require('@testing-library/react');
-const Header = require('../Header');
-
-test('renders header component', () => {
-	render(<Header />);
-	const linkElement = screen.getByText(/header/i);
-	expect(linkElement).toBeInTheDocument();
-});
